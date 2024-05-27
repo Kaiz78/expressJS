c90764487f9c5f52ab9569e4df5abdec1ebab66b
Kaiz78 <luffy4454@gmail.com>
2024-05-27 17:53:55 +0200

    Ajout checkout stripe

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,8 +3,15 @@ import { register, test, login, logout, verifyAccount, createNewPassword, change
 import authMiddleware from '../middleware/authMiddleware.js';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
+import Stripe from 'stripe';
+
+
 
 const router = express.Router();
+
+// Stripe
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 router.post('/register', register);
 router.post('/login', login);
@@ -24,6 +31,32 @@ router.get('/google/callback', passport.authenticate('google', { session: false 
   // Sauvegarde du token dans les cookies
   res.cookie('token', token, { httpOnly: true });
   res.redirect(301, `${process.env.CLIENT_URL}`);
+});
+
+
+
+
+router.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+          {
+              price_data: {
+                  currency: 'usd',
+                  product_data: {
+                      name: 'T-shirt',
+                  },
+                  unit_amount: 2000,
+              },
+              quantity: 1,
+          },
+      ],
+      mode: 'payment',
+      success_url: `${req.protocol}://${req.get('host')}/success`,
+      cancel_url: `${req.protocol}://${req.get('host')}/cancel`,
+  });
+
+  res.json({ url: session.url });
 });
 
 
